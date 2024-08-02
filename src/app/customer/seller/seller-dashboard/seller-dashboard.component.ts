f161f7ee732871936fc6e0646d528bf270bd648e
Rehan Alam <rehanalam9141@gmail.com>
2024-08-02 11:53:14 +0500

    product section completed

--- src/app/customer/seller/seller-dashboard/seller-dashboard.component.ts
+++ src/app/customer/seller/seller-dashboard/seller-dashboard.component.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, numberAttribute, OnInit } from '@angular/core';
+import { CustomerService } from '../../services/customer.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-seller-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './seller-dashboard.component.html',
   styleUrl: './seller-dashboard.component.css'
 })
-export class SellerDashboardComponent {
+export class SellerDashboardComponent implements OnInit {
+
+  order_dashboard_data:any;
+  total_order:any;
+  last_order_date:any;
+
+  product_dashboard_data:any;
+  total_product:number = 0;
+  publish_product:number =0;
+  inactive_product:number = 0;
+  draft_product:number = 0;
+
+  constructor(private customerService:CustomerService, private router:Router) {}
+
+  ngOnInit(): void {
+
+    this.sellerOrderDashboardData();
+    this.sellerProductDashboardData();
+    
+  }
+
+  sellerProductDashboard(){
+    this.router.navigateByUrl('/seller/product')
+  }
+
+  sellerOrderDashboard(){
+    alert('this option for only VIP Condidate')
+  }
+
+  sellerOrderDashboardData(){
+    this.customerService.orderDashboardData().subscribe(data =>{
+      this.order_dashboard_data = data;
+      console.log("Order Dashboard Data",this.order_dashboard_data);
+      this.total_order = Number(this.order_dashboard_data.length);
+      this.last_order_date = this.order_dashboard_data[this.total_order -1].dateTime;
+  },error =>{
+    console.log("my error",error)
+  })
+}
+
+sellerProductDashboardData(){
+  this.customerService.productDashboardData().subscribe(data=>{
+    this.product_dashboard_data = data;
+    for(status in this.product_dashboard_data){
+
+      console.log(this.product_dashboard_data[status].status)
+      if(this.product_dashboard_data[status].status == 'publish'){
+        ++this.publish_product;
+      } else if(this.product_dashboard_data[status].status == 'inactive'){
+        ++this.inactive_product;
+      }else if(this.product_dashboard_data[status].status == 'draft'){
+        ++this.draft_product;
+      }
+
+      ++this.total_product;
+    }
+  }, error=>{
+    console.log("my error",error)
+  })
+}
 
 }
